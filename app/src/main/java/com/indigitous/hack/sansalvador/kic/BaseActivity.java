596a7ff17e6cbb21108bcf84b411f73d1adc2025
Kevin Gomez <kg-diseno@outlook.es>
2016-11-05 15:25:59 -0600

    Setting basic digits

--- app/src/main/java/com/indigitous/hack/sansalvador/kic/BaseActivity.java
+++ app/src/main/java/com/indigitous/hack/sansalvador/kic/BaseActivity.java
@@ -3,11 +3,25 @@ package com.indigitous.hack.sansalvador.kic;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.digits.sdk.android.Digits;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterCore;
+
+import io.fabric.sdk.android.Fabric;
+
 public class BaseActivity extends AppCompatActivity {
+
+    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+    private static final String TWITTER_KEY = "cCkP5xYgUS9E6vykaeEsZM8nZ";
+    private static final String TWITTER_SECRET = "E3Z9CjDhzCiTvXq7dNzWwwhRymgMLUXr5svp9yJpRCmXgiDZQz";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_base);
+
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new TwitterCore(authConfig), new Digits.Builder().build());
+
     }
 }
